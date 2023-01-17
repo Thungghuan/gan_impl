8631db9c12136fe07bf3f3500402b08f32e9af58
Thungghuan <1092887426@qq.com>
2023-01-17 13:15:27 +0800

    feat: add gan code

--- src/gan/main.py
+++ src/gan/main.py
@@ -1,1 +1,248 @@
-print("Original GAN")
+import os
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import torchvision
+from tqdm.auto import tqdm
+
+DIR_NAME = "src/gan"
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim=10, im_dim=784, hidden_dim=128) -> None:
+        super().__init__()
+
+        self.gen = nn.Sequential(
+            self.block(z_dim, hidden_dim),
+            self.block(hidden_dim, hidden_dim * 2),
+            self.block(hidden_dim * 2, hidden_dim * 4),
+            self.block(hidden_dim * 4, hidden_dim * 8),
+            nn.Linear(hidden_dim * 8, im_dim),
+            nn.Sigmoid(),
+        )
+
+    def block(self, input_dim, output_dim):
+        return nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, noise):
+        return self.gen(noise)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, im_dim=784, hidden_dim=128) -> None:
+        super().__init__()
+
+        self.disc = nn.Sequential(
+            self.block(im_dim, hidden_dim * 4),
+            self.block(hidden_dim * 4, hidden_dim * 2),
+            self.block(hidden_dim * 2, hidden_dim),
+            nn.Linear(hidden_dim, 1),
+            nn.Sigmoid(),
+        )
+
+    def block(self, input_dim, output_dim):
+        return nn.Sequential(
+            nn.Linear(input_dim, output_dim), nn.LeakyReLU(0.2, inplace=True)
+        )
+
+    def forward(self, image):
+        return self.disc(image)
+
+
+def get_noise(n_samples, z_dim, device="cpu"):
+    """
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim),
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    """
+    return torch.randn(n_samples, z_dim, device=device)
+
+
+def get_disc_loss(gen, disc, criterion, real, num_images, z_dim, device):
+    """
+    Return the loss of the discriminator given inputs.
+    Parameters:
+        gen: the generator model, which returns an image given z-dimensional noise
+        disc: the discriminator model, which returns a single-dimensional prediction of real/fake
+        criterion: the loss function, which should be used to compare
+               the discriminator's predictions to the ground truth reality of the images
+               (e.g. fake = 0, real = 1)
+        real: a batch of real images
+        num_images: the number of images the generator should produce,
+                which is also the length of the real images
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    Returns:
+        disc_loss: a torch scalar loss value for the current batch
+    """
+
+    #     These are the steps you will need to complete:
+    #       1) Create noise vectors and generate a batch (num_images) of fake images.
+    #            Make sure to pass the device argument to the noise.
+    #       2) Get the discriminator's prediction of the fake image
+    #            and calculate the loss. Don't forget to detach the generator!
+    #            (Remember the loss function you set earlier -- criterion. You need a
+    #            'ground truth' tensor in order to calculate the loss.
+    #            For example, a ground truth tensor for a fake image is all zeros.)
+    #       3) Get the discriminator's prediction of the real image and calculate the loss.
+    #       4) Calculate the discriminator's loss by averaging the real and fake loss
+    #            and set it to disc_loss.
+    #     *Important*: You should NOT write your own loss function here - use criterion(pred, true)!
+    fake_noise = get_noise(num_images, z_dim, device=device)
+    fake = gen(fake_noise)
+    disc_fake_pred = disc(fake.detach())
+    disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+    disc_real_pred = disc(real)
+    disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+    disc_loss = (disc_fake_loss + disc_real_loss) / 2
+
+    return disc_loss
+
+
+def get_gen_loss(gen, disc, criterion, num_images, z_dim, device):
+    """
+    Return the loss of the generator given inputs.
+    Parameters:
+        gen: the generator model, which returns an image given z-dimensional noise
+        disc: the discriminator model, which returns a single-dimensional prediction of real/fake
+        criterion: the loss function, which should be used to compare
+               the discriminator's predictions to the ground truth reality of the images
+               (e.g. fake = 0, real = 1)
+        num_images: the number of images the generator should produce,
+                which is also the length of the real images
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    Returns:
+        gen_loss: a torch scalar loss value for the current batch
+    """
+    #     These are the steps you will need to complete:
+    #       1) Create noise vectors and generate a batch of fake images.
+    #           Remember to pass the device argument to the get_noise function.
+    #       2) Get the discriminator's prediction of the fake image.
+    #       3) Calculate the generator's loss. Remember the generator wants
+    #          the discriminator to think that its fake images are real
+    #     *Important*: You should NOT write your own loss function here - use criterion(pred, true)!
+    fake_noise = get_noise(num_images, z_dim, device=device)
+    fake = gen(fake_noise)
+    disc_fake_pred = disc(fake)
+    gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+
+    return gen_loss
+
+
+def save_result(image_tensor, filename, size=(1, 28, 28)):
+    image_unflat = image_tensor.detach().cpu().view(-1, *size)
+    images = image_unflat[:25]
+
+    torchvision.utils.save_image(
+        images,
+        f"{DIR_NAME}/result/{filename}.png",
+        nrow=5,
+        normalize=True,
+    )
+
+
+def train():
+    n_epochs = 200
+    z_dim = 64
+    display_step = 500
+    batch_size = 128
+    lr = 0.00001
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    criterion = nn.BCEWithLogitsLoss()
+    dataloader = DataLoader(
+        torchvision.datasets.MNIST(
+            "./data/mnist",
+            download=True,
+            transform=torchvision.transforms.ToTensor(),
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    gen = Generator(z_dim).to(device)
+    gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
+    disc = Discriminator().to(device)
+    disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+
+    cur_step = 0
+    mean_generator_loss = 0
+    mean_discriminator_loss = 0
+    test_generator = True  # Whether the generator should be tested
+
+    for epoch in range(n_epochs):
+        iterator = tqdm(dataloader)
+        iterator.set_description(f"Epoch: {epoch + 1}")
+
+        # Dataloader returns the batches
+        for real, _ in iterator:
+            cur_batch_size = len(real)
+
+            # Flatten the batch of real images from the dataset
+            real = real.view(cur_batch_size, -1).to(device)
+
+            ### Update discriminator ###
+            # Zero out the gradients before backpropagation
+            # Calculate discriminator loss
+            # Update gradients
+            # Update optimizer
+            disc_opt.zero_grad()
+            disc_loss = get_disc_loss(
+                gen, disc, criterion, real, cur_batch_size, z_dim, device
+            )
+            disc_loss.backward(retain_graph=True)
+            disc_opt.step()
+
+            # For testing purposes, to keep track of the generator weights
+            if test_generator:
+                old_generator_weights = gen.gen[0][0].weight.detach().clone()
+
+            ### Update generator ###
+            #     Hint: This code will look a lot like the discriminator updates!
+            #     These are the steps you will need to complete:
+            #       1) Zero out the gradients.
+            #       2) Calculate the generator loss, assigning it to gen_loss.
+            #       3) Backprop through the generator: update the gradients and optimizer.
+            gen_opt.zero_grad()
+            gen_loss = get_gen_loss(gen, disc, criterion, cur_batch_size, z_dim, device)
+            gen_loss.backward()
+            gen_opt.step()
+
+            # For testing purposes, to check that your code changes the generator weights
+            if test_generator:
+                try:
+                    assert lr > 0.0000002 or (
+                        gen.gen[0][0].weight.grad.abs().max() < 0.0005 and epoch == 0
+                    )
+                    assert torch.any(
+                        gen.gen[0][0].weight.detach().clone() != old_generator_weights
+                    )
+                except:
+                    print("Runtime tests have failed")
+
+            # Keep track of the average discriminator loss
+            mean_discriminator_loss += disc_loss.item() / display_step
+
+            # Keep track of the average generator loss
+            mean_generator_loss += gen_loss.item() / display_step
+
+            if cur_step % display_step == 0 and cur_step > 0:
+                fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+                fake = gen(fake_noise)
+                save_result(fake, f"fake_{cur_step // display_step}")
+                save_result(real, f"real_{cur_step // display_step}")
+
+            cur_step += 1
+
+
+if __name__ == "__main__":
+    train()
